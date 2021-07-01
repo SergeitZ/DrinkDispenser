5c92f34c4a4fe7622ec46b56fbc86ea8c7d617a8
SergeitZ <sergiofranco6@gmail.com>
2021-07-01 19:37:14 -0400

    initial commit

--- src/com/company/CupHolder.java
+++ src/com/company/CupHolder.java
@@ -2,10 +2,20 @@ package com.company;
 
 public class CupHolder {
 
-    private double volumePerCup;
-    int quantity;
 
-    public static void dispenseCup () {
+    private int volumePerCup;
+    public int quantity = 0;
+    public String small;
+    public String medium;
+    public String large;
+
+
+    public  void dispenseCup (String small, String medium, String large, int volumePerCup, int quantity) {
+        this.small = small;
+        this.medium = medium;
+        this.large = large;
+        this.volumePerCup = volumePerCup;
+        this.quantity = quantity;
 
     }
 
---
+++ src/com/company/Shots.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Shots {
+
+    public String flavor;
+    public int shotAmount;
+    public int shotsAvailable;
+
+    public void dispense (String flavor, int shotAmount) {
+
+        this.flavor = flavor;
+        this.shotAmount = shotAmount;
+
+    }
+
+    public void getVolume (int shotsAvailable) {
+
+    }
+
+}
